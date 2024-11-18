16b4f268a768f21e47512e88e1385bb7cb17af36
Roger Silva <roger.alves.silva84@gmail.com>
2024-11-18 15:37:46 -0300

    Importando as fontes do projeto #2

--- src/componentes/BarraLateral/ItemNavegacao/index.jsx
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -6,6 +6,7 @@ const ItemListaEstilizado = styled.li`
     margin-bottom: 30px;
     cursor: pointer;
     color: ${ props => props.$ativo ? '#7b78e5' : '#d9d9d9' };
+    font-family: ${ props => props.$ativo ? 'GandhiSansBold' : 'GandhiSansRegular' };
     display: flex;
     align-items: center;
     gap: 22px;
